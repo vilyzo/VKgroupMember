baf604f5cccac1e8916e6a7f2c96f25e86aa89e9
Denis <denismishin1@gmail.com>
2023-07-13 00:19:53 +1200

    Добавлен вывод в excel

--- src/HttpPostRequest.java
+++ src/HttpPostRequest.java
@@ -1,7 +1,10 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
@@ -9,6 +12,12 @@ import java.util.Scanner;
 public class HttpPostRequest {
     public static void main(String[] args) {
         System.out.println("@harmesm");
+
+        int i = 0;
+
+        //Создаем excel
+        Workbook wb = new SXSSFWorkbook();
+        Sheet sheet = wb.createSheet("MyShit");
 
         System.out.println("Введите токен(сервисный ключ)");
         Scanner tokenScan = new Scanner(System.in);
@@ -20,6 +29,7 @@ public class HttpPostRequest {
             while (scanner.hasNextLine()) {
                 String tmp = scanner.nextLine();
                 String pubList = tmp.substring(15);
+
 
                 // Создаем URL-адрес для запроса
                 URL url = new URL("https://api.vk.com/method/groups.getMembers?");
@@ -47,16 +57,38 @@ public class HttpPostRequest {
                 }
                 reader.close();
 
+                //Создаем строку
+                Row row = sheet.createRow(i);
+                //Создаем ячейку
+                Cell cell = row.createCell(0);
+                Cell cell2 = row.createCell(1);
+
                 // Выводим ответ от сервера
                 String temp = response.toString();
                 if (temp.contains("group hide members")){
-                    System.out.printf("%-40s%10s%n", publicName,"group hide members");
+                    //System.out.printf("%-40s%10s%n", publicName,"group hide members");
+                    cell.setCellValue(publicName);
+                    cell2.setCellValue("group hide members");
                 }else {
-                    System.out.printf("%-40s%10s%n", publicName,response.substring(21, response.indexOf(",")));
+                    //System.out.printf("%-40s%10s%n", publicName,response.substring(21, response.indexOf(",")));
+                    cell.setCellValue(publicName);
+                    cell2.setCellValue(response.substring(21, response.indexOf(",")));
                 }
-                Thread.sleep(100);
+                i++;
+                if (i % 40 == 0){
+                    System.out.println();
+                } else
+
+                System.out.print(".");
+                //Thread.sleep(100);
             }
             scanner.close();
+            FileOutputStream fos = new FileOutputStream("ThisIsShit.xlsx");
+            wb.write(fos);
+            fos.close();
+            System.out.println();
+            System.out.println("(◕‿◕)");
+            System.out.println("Ready!");
             int c = System.in.read();
         } catch (Exception e) {
             e.printStackTrace();
